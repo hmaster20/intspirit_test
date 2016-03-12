ad2cd8a3d1fbccd57ac7ebf01d4df447f4a9fd5b
hmaster20 <hmaster20@gmail.com>
2016-03-12 23:27:10 +0300

    12.03.2016 first commit

---
+++ scriptC3.js
@@ -0,0 +1,16 @@
+﻿//
+// Challenge #3
+//
+
+function loadData() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (xhttp.status == 200) {
+                document.getElementById("demo").innerHTML = xhttp.responseText;
+                document.getElementById("status").innerHTML = xhttp.status;
+            }
+        };
+        xhttp.open("GET", "http://careers.intspirit.com/endpoint/data_set", true);
+        xhttp.send();
+    }
+
